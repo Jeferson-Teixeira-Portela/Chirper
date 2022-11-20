a62739101e80adc941bc6d78bcc914e291ad610c
Jeferson-Teixeira-Portela <j.teixeira.portela@gmail.com>
2022-11-20 15:26:29 -0300

    menu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     Você está logado!
+                    Clique em chirp no menu acima
                 </div>
             </div>
         </div>
